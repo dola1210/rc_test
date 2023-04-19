c9c8b450bc37d144dba72f4cdd3556e294ad5bd8
dola1210 <dola1210@gmail.com>
2023-04-19 12:03:35 +0800

    a

--- js/ida.js
+++ js/ida.js
@@ -1,5 +1,5 @@
 $(function () {
-    csmapi.set_endpoint ('https://2.iottalk.tw');
+    csmapi.set_endpoint ('https://6.iottalk.tw');
     var profile = {
         'dm_name': 'Remote_control',
         'idf_list': [Keypad1],
